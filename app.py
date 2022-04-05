77bbb522391a063f45800a7e1b45fa6e34928811
Ziling(Lizzy) Li <91296853+zilingli1@users.noreply.github.com>
2022-04-04 22:35:08 -0500

    third commit

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Vertical Tank Maintenance')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', pageTitle='AboutVTM')
 
 @app.route('/estimate')
 def estimate():
-    return render_template('estimate.html')
+    return render_template('estimate.html', pageTitle='Tank Painting Estimate')
 
 if __name__ == '__main__':
     app.run(debug=True)
