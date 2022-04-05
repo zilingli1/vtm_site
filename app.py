8500658aba3382e7665bb5a9fe0276cc57fe1930
Ziling(Lizzy) Li <91296853+zilingli1@users.noreply.github.com>
2022-04-04 19:44:00 -0500

    vtm second commit

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
